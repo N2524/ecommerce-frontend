86682c85ff0681a26f0ebfbd1b2e9f81bba6a195
N2524 <215958224+N2524@users.noreply.github.com>
2025-08-13 20:01:10 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("E-Commerce Website Loaded");
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.getElementById('hamburger');
+  const navbar = document.getElementById('navbar');
+
+  hamburger.addEventListener('click', function () {
+    navbar.classList.toggle('open');
+  });
+});
+
